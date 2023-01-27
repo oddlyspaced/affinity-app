e02001e628abb3026a1c967ccddde386994a9e7c
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-27 21:44:15 +0530

    create app-provider module

---
+++ app-provider/src/main/java/com/oddlyspaced/surge/app_provider/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.oddlyspaced.surge.app_provider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
