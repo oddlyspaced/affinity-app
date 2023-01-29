55b9bfa77e4c02ac8b04f274064e17b91e70d283
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-29 15:37:03 +0530

    customer: App: setup constants for API url

--- app-customer/src/main/java/com/oddlyspaced/surge/affinity/App.kt
+++ app-customer/src/main/java/com/oddlyspaced/surge/affinity/App.kt
@@ -5,4 +5,9 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application() {
+    companion object {
+        private const val API_URL_LINK = "http://192.168.29.36"
+        private const val API_URL_PORT = "4444"
+        const val API_URL = "$API_URL_LINK:$API_URL_PORT"
+    }
 }
