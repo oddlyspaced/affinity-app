e409280ddc2d0867c2eb9aac56f654d26af519cf
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-29 22:04:59 +0530

    customer: ProviderDetailsFragment: bind provider details to ui

--- app-customer/src/main/java/com/oddlyspaced/surge/affinity/fragment/ProviderDetailsFragment.kt
+++ app-customer/src/main/java/com/oddlyspaced/surge/affinity/fragment/ProviderDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.oddlyspaced.surge.affinity.databinding.LayoutBottomSheetProviderDetailsBinding
 
@@ -16,9 +17,19 @@ class ProviderDetailsFragment: BottomSheetDialogFragment() {
     }
 
     private lateinit var binding: LayoutBottomSheetProviderDetailsBinding
+    private val args: ProviderDetailsFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = LayoutBottomSheetProviderDetailsBinding.inflate(inflater, container, false)
+        init()
         return binding.root
+    }
+
+    private fun init() {
+        val provider = args.provider
+        binding.apply {
+            txProviderInfoName.text = provider.name
+            txProviderInfoDistance.text = "13 km away" // todo: calculate real distance
+        }
     }
 }
