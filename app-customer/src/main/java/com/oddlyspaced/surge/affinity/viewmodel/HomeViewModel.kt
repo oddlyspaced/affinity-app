c6bbc14d30ff4265e4b526ac0a0c87aa48858d9e
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-29 16:15:59 +0530

    customer: HomeViewModel: setup passthrough viewmodel for provider

---
+++ app-customer/src/main/java/com/oddlyspaced/surge/affinity/viewmodel/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.oddlyspaced.surge.affinity.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.oddlyspaced.surge.affinity.modal.Provider
+import com.oddlyspaced.surge.affinity.repository.ProviderRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private val repo: ProviderRepository): ViewModel() {
+    val providers = repo.providers()
+}
