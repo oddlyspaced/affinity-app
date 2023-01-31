26f051d58929c3fc0fb10d2a061d11dbee166a2c
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-31 13:44:59 +0530

    provider: HomeFragment: test locationfetcher lib

--- app-provider/src/main/java/com/oddlyspaced/surge/app_provider/fragment/HomeFragment.kt
+++ app-provider/src/main/java/com/oddlyspaced/surge/app_provider/fragment/HomeFragment.kt
@@ -1,34 +1,70 @@
 package com.oddlyspaced.surge.app_provider.fragment
 
-import android.graphics.Color
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
+import arrow.core.left
+import com.freelapp.libs.locationfetcher.LocationFetcher
+import com.freelapp.libs.locationfetcher.locationFetcher
+import com.google.android.gms.location.LocationRequest
 import com.oddlyspaced.surge.app_provider.App
 import com.oddlyspaced.surge.app_provider.BuildConfig
 import com.oddlyspaced.surge.app_provider.R
 import com.oddlyspaced.surge.app_provider.databinding.FragmentHomeBinding
+import com.oddlyspaced.surge.app_provider.util.Logger
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.CustomZoomButtonsController
-import org.osmdroid.views.overlay.Marker
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.Duration.Companion.seconds
 
 @AndroidEntryPoint
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
+    val locationFetcher = locationFetcher("We need your permission to use your location for showing nearby items") {
+        fastestInterval = 5.seconds
+        interval = 15.seconds
+        maxWaitTime = 2.minutes
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        smallestDisplacement = 50f
+        isWaitForAccurateLocation = false
+        providers = listOf(
+            LocationFetcher.Provider.GPS,
+            LocationFetcher.Provider.Network,
+            LocationFetcher.Provider.Fused,
+        )
+        numUpdates = Int.MAX_VALUE
+        debug = BuildConfig.DEBUG
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentHomeBinding.bind(view)
 
         initOSMDroid()
+        CoroutineScope(Dispatchers.IO).launch {
+            ok()
+        }
+
+
 //        markCurrentLocation()
 //        init()
+    }
+
+    private suspend fun ok() {
+        locationFetcher.location.collectLatest {
+            it.fold({ error ->
+                Logger.d("ERROR: $error")
+            }, { location ->
+                Logger.d("LOCATION: " + location.latitude + " " + location.longitude)
+            })
+        }
     }
 
     // handles runtime map configuration
