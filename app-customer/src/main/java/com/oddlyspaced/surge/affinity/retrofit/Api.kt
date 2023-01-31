389070dd8fc031c864f6e9c4459ef90d6d606046
oddlyspaced <oddlyspaced1@gmail.com>
2023-01-31 17:11:35 +0530

    customer: Api: use common modal package

--- app-customer/src/main/java/com/oddlyspaced/surge/affinity/retrofit/Api.kt
+++ app-customer/src/main/java/com/oddlyspaced/surge/affinity/retrofit/Api.kt
@@ -1,12 +1,12 @@
 package com.oddlyspaced.surge.affinity.retrofit
 
-import com.oddlyspaced.surge.affinity.modal.Provider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityRetainedComponent
 import retrofit2.http.GET
 import retrofit2.http.POST
+import com.oddlyspaced.surge.app_common.modal.*
 
 interface Api {
     @GET("/provider/all")
