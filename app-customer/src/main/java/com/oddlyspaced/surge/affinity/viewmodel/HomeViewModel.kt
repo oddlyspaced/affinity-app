1bf75b0009910e581c4df542df774472a5ccc483
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-01 22:06:06 +0530

    customer: HomeViewModel: use map for storing location

--- app-customer/src/main/java/com/oddlyspaced/surge/affinity/viewmodel/HomeViewModel.kt
+++ app-customer/src/main/java/com/oddlyspaced/surge/affinity/viewmodel/HomeViewModel.kt
@@ -3,6 +3,7 @@ package com.oddlyspaced.surge.affinity.viewmodel
 import androidx.lifecycle.ViewModel
 import com.oddlyspaced.surge.affinity.repository.LocationRepository
 import com.oddlyspaced.surge.affinity.repository.ProviderRepository
+import com.oddlyspaced.surge.app_common.modal.Address
 import com.oddlyspaced.surge.app_common.modal.Location
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,8 +11,10 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repo: ProviderRepository, private val locationRepository: LocationRepository): ViewModel() {
     val providers = repo.providers()
-    var pickupLocation: Location? = null
-    var pickupLocationAddress = ""
-    var dropLocation: Location? = null
+    val selectedLocation = hashMapOf<LocationType, Address>()
     fun addressFromLocation(location: Location, zoom: Int) = locationRepository.address(location, zoom)
+}
+
+enum class LocationType {
+    PICKUP, DROP
 }
