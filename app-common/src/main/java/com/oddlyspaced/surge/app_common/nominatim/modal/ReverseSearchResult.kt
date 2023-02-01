2ba57c4cf1bd8e56d7df8ab472cbd4d7e5d48bf2
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-01 19:43:49 +0530

    common: ReverseSearchResult: only keep display name

--- app-common/src/main/java/com/oddlyspaced/surge/app_common/nominatim/modal/ReverseSearchResult.kt
+++ app-common/src/main/java/com/oddlyspaced/surge/app_common/nominatim/modal/ReverseSearchResult.kt
@@ -3,19 +3,19 @@ package com.oddlyspaced.surge.app_common.nominatim.modal
 import com.google.gson.annotations.SerializedName
 
 data class ReverseSearchResult(
-    @SerializedName("place_id") var placeId: Int? = null,
-    @SerializedName("licence") var licence: String? = null,
-    @SerializedName("osm_type") var osmType: String? = null,
-    @SerializedName("osm_id") var osmId: Int? = null,
-    @SerializedName("lat") var lat: String? = null,
-    @SerializedName("lon") var lon: String? = null,
-    @SerializedName("place_rank") var placeRank: Int? = null,
-    @SerializedName("category") var category: String? = null,
-    @SerializedName("type") var type: String? = null,
-    @SerializedName("importance") var importance: Double? = null,
-    @SerializedName("addresstype") var addresstype: String? = null,
-    @SerializedName("name") var name: String? = null,
+//    @SerializedName("place_id") var placeId: Int? = null,
+//    @SerializedName("licence") var licence: String? = null,
+//    @SerializedName("osm_type") var osmType: String? = null,
+//    @SerializedName("osm_id") var osmId: Int? = null,
+//    @SerializedName("lat") var lat: String? = null,
+//    @SerializedName("lon") var lon: String? = null,
+//    @SerializedName("place_rank") var placeRank: Int? = null,
+//    @SerializedName("category") var category: String? = null,
+//    @SerializedName("type") var type: String? = null,
+//    @SerializedName("importance") var importance: Double? = null,
+//    @SerializedName("addresstype") var addresstype: String? = null,
+//    @SerializedName("name") var name: String? = null,
     @SerializedName("display_name") var displayName: String? = null,
-    @SerializedName("address") var address: Address? = Address(),
-    @SerializedName("boundingbox") var boundingbox: ArrayList<String> = arrayListOf()
+//    @SerializedName("address") var address: Address? = Address(),
+//    @SerializedName("boundingbox") var boundingbox: ArrayList<String> = arrayListOf()
 )
