a649791c7c7f5ea637caefa808a1a7483f33e3e4
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-01 19:16:19 +0530

    customer: HomeViewModel: add method for fetching address from location

--- app-customer/src/main/java/com/oddlyspaced/surge/affinity/viewmodel/HomeViewModel.kt
+++ app-customer/src/main/java/com/oddlyspaced/surge/affinity/viewmodel/HomeViewModel.kt
@@ -1,16 +1,16 @@
 package com.oddlyspaced.surge.affinity.viewmodel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.oddlyspaced.surge.app_common.modal.Provider
+import com.oddlyspaced.surge.affinity.repository.LocationRepository
 import com.oddlyspaced.surge.affinity.repository.ProviderRepository
 import com.oddlyspaced.surge.app_common.modal.Location
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val repo: ProviderRepository): ViewModel() {
+class HomeViewModel @Inject constructor(private val repo: ProviderRepository, private val locationRepository: LocationRepository): ViewModel() {
     val providers = repo.providers()
     var pickupLocation: Location? = null
     var dropLocation: Location? = null
+    fun addressFromLocation(location: Location, zoom: Int) = locationRepository.address(location, zoom)
 }
