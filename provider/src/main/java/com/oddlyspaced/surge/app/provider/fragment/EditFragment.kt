8910ee62819920f11b1418dbfc8e0096168d3e5e
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-02 22:49:22 +0530

    provider: EditFragment: select source point and functional radius

--- provider/src/main/java/com/oddlyspaced/surge/app/provider/fragment/EditFragment.kt
+++ provider/src/main/java/com/oddlyspaced/surge/app/provider/fragment/EditFragment.kt
@@ -9,15 +9,18 @@ import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.freelapp.libs.locationfetcher.LocationFetcher
 import com.freelapp.libs.locationfetcher.locationFetcher
 import com.google.android.gms.location.LocationRequest
+import com.google.android.material.slider.Slider
 import com.oddlyspaced.surge.app.common.AffinityConfiguration
 import com.oddlyspaced.surge.app.common.Logger
 import com.oddlyspaced.surge.app.common.asGeoPoint
 import com.oddlyspaced.surge.app.common.asLocation
 import com.oddlyspaced.surge.app.common.modal.Address
 import com.oddlyspaced.surge.app.common.modal.Location
+import com.oddlyspaced.surge.app.common.modal.asGeoPoint
 import com.oddlyspaced.surge.app.provider.BuildConfig
 import com.oddlyspaced.surge.app.provider.R
 import com.oddlyspaced.surge.app.provider.databinding.FragmentEditBinding
@@ -33,6 +36,7 @@ import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Overlay
+import org.osmdroid.views.overlay.Polygon
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 
@@ -61,10 +65,11 @@ class EditFragment: Fragment(R.layout.fragment_edit) {
         debug = BuildConfig.DEBUG
     }
 
+    /**
+     * called when view is rendered for display
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentEditBinding.bind(view)
-        // edit -> active location of provider to serve
-        // make changes to backend to accommodate these positional changes
         initOSMDroid()
         CoroutineScope(Dispatchers.IO).launch {
             markCurrentLocation()
@@ -73,14 +78,40 @@ class EditFragment: Fragment(R.layout.fragment_edit) {
         setupTouchTargetOverlay()
     }
 
-    // handles runtime map configuration
-    private fun initOSMDroid() {
-        // setup map configuration
-        Configuration.getInstance().let { config ->
-            config.load(requireContext(), PreferenceManager.getDefaultSharedPreferences(requireContext()))
-            config.userAgentValue = BuildConfig.APPLICATION_ID
+    private fun init() {
+        binding.selectEditLocation.txSelectLocation.text = "Pick center of serving area"
+        binding.selectEditLocation.imgLocation.setColorFilter(Color.BLACK)
+
+        binding.sliderEditDistance.addOnChangeListener { _, value, fromUser ->
+            if (fromUser) {
+                binding.textSearchDistance.text = "${value.toInt()} km"
+            }
+        }
+        binding.sliderEditDistance.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {}
+
+            override fun onStopTrackingTouch(slider: Slider) {
+                if (currentAddress == null) {
+                    Toast.makeText(requireContext(), "Please select a source point by tapping on map first!", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    createCircleAroundPoint(currentAddress!!.location.asGeoPoint(), slider.value.toDouble())
+                }
+            }
+        })
+        binding.cardEditSave.setOnClickListener {
+            homeViewModel.sourcePointAddress = currentAddress
+            homeViewModel.sourcePointWorkingRadius = binding.sliderEditDistance.value.toInt()
+            // post this to server here
+            findNavController().popBackStack()
         }
 
+    }
+
+    /**
+     * handles runtime map configuration
+     */
+    private fun initOSMDroid() {
         // setting up map
         binding.map.apply {
             setUseDataConnection(true)
@@ -93,6 +124,27 @@ class EditFragment: Fragment(R.layout.fragment_edit) {
         binding.map.controller.setZoom(AffinityConfiguration.DEFAULT_MAP_ZOOM)
     }
 
+    // handle touches on map
+    private fun setupTouchTargetOverlay() {
+        val overlay = object : Overlay() {
+            override fun onSingleTapConfirmed(e: MotionEvent, mapView: MapView): Boolean {
+                val projection = binding.map.projection
+                val loc = projection.fromPixels(e.x.toInt(), e.y.toInt())
+                setAddressForLocation(loc.asGeoPoint().asLocation())
+                Logger.d("${loc.latitude} | ${loc.longitude}")
+                binding.map.controller.setCenter(loc.asGeoPoint())
+                addMarker(loc.asGeoPoint())
+                return true
+            }
+        }
+        binding.map.overlays.add(overlay)
+    }
+
+    //// utils
+
+    /**
+     * location fetcher lib implementation to fetch current location and then mark it on the map as marker
+     */
     private suspend fun markCurrentLocation() {
         locationFetcher.location.collectLatest {
             it.fold({ error ->
@@ -118,31 +170,6 @@ class EditFragment: Fragment(R.layout.fragment_edit) {
                 }
             })
         }
-
-    }
-
-    // handle touches on map
-    private fun setupTouchTargetOverlay() {
-        val overlay = object : Overlay() {
-            override fun onSingleTapConfirmed(e: MotionEvent, mapView: MapView): Boolean {
-                val projection = binding.map.projection
-                val loc = projection.fromPixels(e.x.toInt(), e.y.toInt())
-                setAddressForLocation(loc.asGeoPoint().asLocation())
-                Logger.d("${loc.latitude} | ${loc.longitude}")
-                binding.map.controller.setCenter(loc.asGeoPoint())
-                addMarker(loc.asGeoPoint())
-                return true
-            }
-        }
-        binding.map.overlays.add(overlay)
-    }
-
-    private fun init() {
-        binding.selectEditLocation.txSelectLocation.text = "Pick center of serving area"
-        binding.selectEditLocation.imgLocation.setColorFilter(Color.BLACK)
-
-
-
     }
 
     private fun addMarker(point: GeoPoint) {
@@ -155,6 +182,7 @@ class EditFragment: Fragment(R.layout.fragment_edit) {
             setInfoWindow(null)
         }
         binding.map.overlays.add(currentMarker)
+        createCircleAroundPoint(point, binding.sliderEditDistance.value.toDouble())
     }
 
     private fun setAddressForLocation(location: Location) {
@@ -164,4 +192,33 @@ class EditFragment: Fragment(R.layout.fragment_edit) {
             currentAddress = Address(location, result.displayName ?: "${location.lat}, ${location.lon}")
         }
     }
+
+    private val polygonGeoPoints = arrayListOf<GeoPoint>()
+    private lateinit var areaPolygon: Polygon
+
+    private fun createCircleAroundPoint(point: GeoPoint, radius: Double) {
+        Logger.d("Drawing circle for $point")
+        polygonGeoPoints.clear()
+        for (i in 0 until 360) {
+            polygonGeoPoints.add(point.destinationPoint(radius * 1000, i.toDouble()))
+        }
+        handlePolygon()
+    }
+
+    private fun handlePolygon() {
+        if (this::areaPolygon.isInitialized) {
+            binding.map.overlayManager.remove(areaPolygon)
+        }
+        areaPolygon = Polygon().apply {
+            title = "Sample Title"
+            fillPaint.color = Color.BLUE
+            fillPaint.alpha = 128
+            outlinePaint.color = Color.BLACK
+            isVisible = true
+            points = polygonGeoPoints
+        }
+        binding.map.overlayManager.add(areaPolygon)
+        binding.map.invalidate()
+    }
+
 }
