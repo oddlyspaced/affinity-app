0c47a6c88fcb551be76e39843986d4b27dfa890f
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-03 21:31:06 +0530

    common: CommonExtension: add extension for copying config

--- common/src/main/java/com/oddlyspaced/surge/app/common/CommonExtension.kt
+++ common/src/main/java/com/oddlyspaced/surge/app/common/CommonExtension.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.location.Location
 import android.util.Log
 import android.widget.Toast
+import com.freelapp.libs.locationfetcher.LocationFetcher
 import org.osmdroid.api.IGeoPoint
 import org.osmdroid.util.GeoPoint
 
@@ -19,3 +20,15 @@ fun Context.toast(text: String, length: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, text, length).show()
 }
 fun GeoPoint.asLocation() = com.oddlyspaced.surge.app.common.modal.Location(this.latitude, this.longitude)
+
+fun LocationFetcher.Config.applyFrom(config: LocationFetcher.Config) {
+    this.fastestInterval = config.fastestInterval
+    this.interval = config.interval
+    this.maxWaitTime = config.maxWaitTime
+    this.priority = config.priority
+    this.smallestDisplacement = config.smallestDisplacement
+    this.isWaitForAccurateLocation = config.isWaitForAccurateLocation
+    this.providers = config.providers
+    this.numUpdates = config.numUpdates
+    this.debug = config.debug
+}
