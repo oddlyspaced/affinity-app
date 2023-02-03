4375bc7a4c9dea06503a938497955a520d225de7
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-03 16:42:10 +0530

    customer: ProviderRepository: modify provider method to directly pass

--- common/src/main/java/com/oddlyspaced/surge/app/common/repository/ProviderRepository.kt
+++ common/src/main/java/com/oddlyspaced/surge/app/common/repository/ProviderRepository.kt
@@ -1,22 +1,8 @@
 package com.oddlyspaced.surge.app.common.repository
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import com.oddlyspaced.surge.app.common.modal.Provider
 import com.oddlyspaced.surge.app.common.retrofit.Api
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ProviderRepository @Inject constructor(private val api: Api) {
-
-    fun providers(): LiveData<ArrayList<Provider>> {
-        val data = MutableLiveData<ArrayList<Provider>>()
-        CoroutineScope(Dispatchers.IO).launch {
-            data.postValue(api.fetchAllProviders())
-        }
-        return data
-    }
-
+    suspend fun providers() = api.fetchAllProviders()
 }
