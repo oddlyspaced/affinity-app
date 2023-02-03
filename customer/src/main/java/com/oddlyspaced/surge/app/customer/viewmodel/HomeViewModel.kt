90c6ddc64d15c5989de029005410b6979dbca26f
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-03 16:56:01 +0530

    customer: HomeViewModel: cache provider info in view model

--- customer/src/main/java/com/oddlyspaced/surge/app/customer/viewmodel/HomeViewModel.kt
+++ customer/src/main/java/com/oddlyspaced/surge/app/customer/viewmodel/HomeViewModel.kt
@@ -1,16 +1,38 @@
 package com.oddlyspaced.surge.app.customer.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.oddlyspaced.surge.app.common.Logger
 import com.oddlyspaced.surge.app.common.repository.LocationRepository
 import com.oddlyspaced.surge.app.common.repository.ProviderRepository
 import com.oddlyspaced.surge.app.common.modal.Address
 import com.oddlyspaced.surge.app.common.modal.Location
+import com.oddlyspaced.surge.app.common.modal.Provider
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repo: ProviderRepository, private val locationRepository: LocationRepository): ViewModel() {
-    val providers = repo.providers()
+
+    private var providersFetchedAt = 0L
+    private val _providers = MutableLiveData<ArrayList<Provider>>()
+    val providers: LiveData<ArrayList<Provider>>
+    get() {
+        if (System.currentTimeMillis() - providersFetchedAt > 5 * 1000L) {
+            // need to refresh
+            Logger.d("Fetching Providers!")
+            CoroutineScope(Dispatchers.IO).launch {
+                _providers.postValue(repo.providers())
+                providersFetchedAt = System.currentTimeMillis()
+            }
+        }
+       return _providers
+    }
+
     val selectedLocation = hashMapOf<LocationType, Address>()
     fun addressFromLocation(location: Location, zoom: Int) = locationRepository.address(location, zoom)
 }
