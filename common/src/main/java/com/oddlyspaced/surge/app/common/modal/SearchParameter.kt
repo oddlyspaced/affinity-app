c1d1f0fb8c8dc34735023aa7f8ed50b0f3a64202
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-04 14:32:13 +0530

    common: SearchParameter: flatten out params class

--- common/src/main/java/com/oddlyspaced/surge/app/common/modal/SearchParameter.kt
+++ common/src/main/java/com/oddlyspaced/surge/app/common/modal/SearchParameter.kt
@@ -3,12 +3,13 @@ package com.oddlyspaced.surge.app.common.modal
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-
 @Parcelize
 class SearchParameter(
     val limitCount: Int = 10,
     val limitDistance: Int = 10,
-    val pickupPoint: Location,
-    val dropPoint: Location,
-    val filterServices: ArrayList<String>? = arrayListOf()
+    val pickupLat: Double,
+    val pickupLon: Double,
+    val dropLat: Double,
+    val dropLon: Double,
+    val filterServices: ArrayList<String>
 ): Parcelable
