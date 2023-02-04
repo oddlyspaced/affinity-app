d4167b20792fffb4c8fb4dbe1b4f1d16464f8936
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-04 14:32:40 +0530

    common: Api: refactor + remove filter list

--- common/src/main/java/com/oddlyspaced/surge/app/common/retrofit/Api.kt
+++ common/src/main/java/com/oddlyspaced/surge/app/common/retrofit/Api.kt
@@ -11,8 +11,17 @@ import retrofit2.http.Query
 interface Api {
     @GET("/provider/all")
     suspend fun fetchAllProviders(): ArrayList<Provider>
+
     @GET("/provider/services")
     suspend fun fetchServiceTags(): ArrayList<Service>
-    @GET("/provider/search")
-    suspend fun searchProviders(@Query("limitCount") limitCount: Int, @Query("limitDistance") limitDistance: Int, @Query("pickupPoint") pickupPoint: Location, @Query("dropPoint") dropPoint: Location, @Query("filterServices") filterServices: ArrayList<String>): ArrayList<Provider>
+
+    @POST("/provider/search")
+    suspend fun searchProviders(
+        @Query("limitCount") limitCount: Int,
+        @Query("limitDistance") limitDistance: Int,
+        @Query("pickupLat") pickupLat: Double,
+        @Query("pickupLon") pickupLon: Double,
+        @Query("dropLat") dropLat: Double,
+        @Query("dropLon") dropLon: Double,
+    ): ArrayList<Provider>
 }
