40d4a4bb58ffa7f0d56200c2ca65c218ebb90af5
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-04 15:44:48 +0530

    customer: ProviderListAdapter, item_provider_detail: fix call button

--- customer/src/main/java/com/oddlyspaced/surge/app/customer/adapter/ProviderListAdapter.kt
+++ customer/src/main/java/com/oddlyspaced/surge/app/customer/adapter/ProviderListAdapter.kt
@@ -1,15 +1,29 @@
 package com.oddlyspaced.surge.app.customer.adapter
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.chip.Chip
 import com.oddlyspaced.surge.app.common.modal.Provider
 import com.oddlyspaced.surge.app.customer.databinding.ItemProviderDetailBinding
+
 
 class ProviderListAdapter(private val data: ArrayList<Provider>): RecyclerView.Adapter<ProviderListAdapter.ViewHolder>() {
     inner class ViewHolder(private val item: ItemProviderDetailBinding): RecyclerView.ViewHolder(item.root) {
         fun bind(param: Provider) {
-
+            item.txProviderInfoName.text = param.name
+            param.services.forEach { service ->
+                item.chipGroupProviderInfoService.addView(Chip(item.root.context).apply {
+                    text = service
+                })
+            }
+            item.fabProviderCall.setOnClickListener {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:${param.phone.phoneNumber}")
+                item.root.context.startActivity(intent)
+            }
         }
     }
 
