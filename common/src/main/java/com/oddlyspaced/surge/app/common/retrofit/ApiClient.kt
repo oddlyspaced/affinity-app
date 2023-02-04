896b3eef5aad0b78339abcd363613e16208c0fcf
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-04 14:33:19 +0530

    common: ApiClient: add interceptor for loggin

--- common/src/main/java/com/oddlyspaced/surge/app/common/retrofit/ApiClient.kt
+++ common/src/main/java/com/oddlyspaced/surge/app/common/retrofit/ApiClient.kt
@@ -3,6 +3,7 @@ package com.oddlyspaced.surge.app.common.retrofit
 import com.google.gson.GsonBuilder
 import com.oddlyspaced.surge.app.common.AffinityConfiguration
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -11,12 +12,15 @@ class ApiClient {
     private lateinit var client: Api
     fun getApiClient(): Api {
         if (!this::client.isInitialized) {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
             val gson = GsonBuilder()
                 .setLenient()
                 .create()
             val okHttpClient = OkHttpClient.Builder()
                 .readTimeout(5000, TimeUnit.SECONDS)
                 .connectTimeout(100, TimeUnit.SECONDS)
+                .addInterceptor(loggingInterceptor)
                 .build()
             client = Retrofit.Builder()
                 .baseUrl(AffinityConfiguration.API_URL)
