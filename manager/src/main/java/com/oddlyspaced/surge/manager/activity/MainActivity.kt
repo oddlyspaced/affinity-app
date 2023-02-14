988a1a80adc25cc39866a763e0a5905e2f7009c2
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-15 00:31:59 +0530

    manager: MainActivity, AndroidManifest: setup nav host fragment

---
+++ manager/src/main/java/com/oddlyspaced/surge/manager/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.surge.manager.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.surge.manager.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
