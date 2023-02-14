e758d0ddb77c2793b516a39952f146d996db24d1
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-15 00:31:28 +0530

    manager: init home fragment

---
+++ manager/src/main/java/com/oddlyspaced/surge/manager/fragment/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.oddlyspaced.surge.manager.fragment
+
+import androidx.fragment.app.Fragment
+import com.oddlyspaced.surge.manager.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
