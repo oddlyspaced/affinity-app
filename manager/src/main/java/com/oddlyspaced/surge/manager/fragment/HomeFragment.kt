b8359b8d60f80d1dd6e14f37687805ed3cc03181
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-17 15:50:11 +0530

    manager: HomeFragment: init home base layout

--- manager/src/main/java/com/oddlyspaced/surge/manager/fragment/HomeFragment.kt
+++ manager/src/main/java/com/oddlyspaced/surge/manager/fragment/HomeFragment.kt
@@ -1,7 +1,20 @@
 package com.oddlyspaced.surge.manager.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.oddlyspaced.surge.manager.R
+import com.oddlyspaced.surge.manager.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding = FragmentHomeBinding.bind(view)
+//        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToAddFragment())
+    }
 }
