45ced4ceecbb87a6b4f51d0340e7dc2f391f2c3e
oddlyspaced <oddlyspaced1@gmail.com>
2023-02-18 18:17:27 +0530

    manager: ProviderListAdapter: create provider list adapter

---
+++ manager/src/main/java/com/oddlyspaced/surge/manager/adapter/ProviderListAdapter.kt
@@ -0,0 +1,30 @@
+package com.oddlyspaced.surge.manager.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.oddlyspaced.surge.app.common.modal.Provider
+import com.oddlyspaced.surge.manager.databinding.ItemProviderBinding
+
+class ProviderListAdapter(private val items: List<Provider>) : RecyclerView.Adapter<ProviderListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(private val binding: ItemProviderBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Provider) {
+            binding.txItemProviderName.text = data.name
+            binding.txItemProviderPhone.text = "${data.phone.countryCode} ${data.phone.phoneNumber}"
+            binding.txItemProviderServices.text = data.services.joinToString(", ")
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemProviderBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
