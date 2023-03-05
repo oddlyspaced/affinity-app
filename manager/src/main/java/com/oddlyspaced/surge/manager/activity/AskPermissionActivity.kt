1b32325ac9012efee13cb130372cefbd338febf9
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-05 11:45:19 +0530

    manager: AskPermissionActivity: init

---
+++ manager/src/main/java/com/oddlyspaced/surge/manager/activity/AskPermissionActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.surge.manager.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.surge.manager.R
+
+class AskPermissionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask_permission)
+    }
+}
