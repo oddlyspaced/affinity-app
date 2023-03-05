a974d9586e9751ccecf336173d9df3bbe7eb1106
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-05 11:53:44 +0530

    common: CheckActivity: init

---
+++ common/src/main/java/com/oddlyspaced/surge/app/common/activity/CheckActivity.kt
@@ -0,0 +1,12 @@
+package com.oddlyspaced.surge.app.common.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.oddlyspaced.surge.app.common.R
+
+class CheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check)
+    }
+}
