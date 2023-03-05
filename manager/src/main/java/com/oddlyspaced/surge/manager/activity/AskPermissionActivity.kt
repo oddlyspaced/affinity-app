2539f6e59e1d039b2b66ce7a387143461f6f09ae
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-05 11:52:43 +0530

    Revert "manager: AskPermissionActivity: use view binding"
    This reverts commit ce86ed1879b01c23d921d2726517272f4f26f96f.

--- manager/src/main/java/com/oddlyspaced/surge/manager/activity/AskPermissionActivity.kt
+++ manager/src/main/java/com/oddlyspaced/surge/manager/activity/AskPermissionActivity.kt
@@ -3,14 +3,10 @@ package com.oddlyspaced.surge.manager.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.surge.manager.R
-import com.oddlyspaced.surge.manager.databinding.ActivityAskPermissionBinding
 
 class AskPermissionActivity : AppCompatActivity() {
-
-    private val binding by lazy { ActivityAskPermissionBinding.inflate(layoutInflater) }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_ask_permission)
     }
 }
