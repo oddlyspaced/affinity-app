ce86ed1879b01c23d921d2726517272f4f26f96f
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-05 11:48:58 +0530

    manager: AskPermissionActivity: use view binding

--- manager/src/main/java/com/oddlyspaced/surge/manager/activity/AskPermissionActivity.kt
+++ manager/src/main/java/com/oddlyspaced/surge/manager/activity/AskPermissionActivity.kt
@@ -3,10 +3,14 @@ package com.oddlyspaced.surge.manager.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.surge.manager.R
+import com.oddlyspaced.surge.manager.databinding.ActivityAskPermissionBinding
 
 class AskPermissionActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAskPermissionBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ask_permission)
+        setContentView(binding.root)
     }
 }
