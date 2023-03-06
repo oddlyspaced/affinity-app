05204daaa0cb7d339fcbe7ab032a6c8463ad0d0a
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-07 04:41:50 +0530

    provider: ProviderViewModel: add ping method

--- provider/src/main/java/com/oddlyspaced/surge/app/provider/viewmodel/ProviderViewModel.kt
+++ provider/src/main/java/com/oddlyspaced/surge/app/provider/viewmodel/ProviderViewModel.kt
@@ -3,9 +3,12 @@ package com.oddlyspaced.surge.app.provider.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.oddlyspaced.surge.app.common.Logger
 import com.oddlyspaced.surge.app.common.modal.Address
 import com.oddlyspaced.surge.app.common.modal.Location
 import com.oddlyspaced.surge.app.common.modal.Provider
+import com.oddlyspaced.surge.app.common.modal.ResponseError
+import com.oddlyspaced.surge.app.common.repository.GeneralRepository
 import com.oddlyspaced.surge.app.common.repository.LocationRepository
 import com.oddlyspaced.surge.app.common.repository.ProviderRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -16,10 +19,12 @@ import javax.inject.Inject
 
 
 @HiltViewModel
-class ProviderViewModel @Inject constructor(private val repo: ProviderRepository, private val locationRepository: LocationRepository): ViewModel() {
+class ProviderViewModel @Inject constructor(private val repo: ProviderRepository, private val locationRepository: LocationRepository, private val generalRepository: GeneralRepository): ViewModel() {
     var sourcePointAddress: Address? = null
     var sourcePointWorkingRadius: Double = -1.0
+
     fun addressFromLocation(location: Location, zoom: Int) = locationRepository.address(location, zoom)
+
     fun getProviderInfo(id: Int): LiveData<Provider> {
         val _provider = MutableLiveData<Provider>()
         CoroutineScope(Dispatchers.IO).launch {
@@ -27,15 +32,31 @@ class ProviderViewModel @Inject constructor(private val repo: ProviderRepository
         }
         return _provider
     }
+
     fun updateProviderSourceArea(id: Int, sourcePoint: Location, radius: Double) {
         CoroutineScope(Dispatchers.IO).launch {
             repo.saveProviderSource(1, sourcePoint, radius)
         }
     }
+
     fun updateProviderStatus(id: Int, status: Boolean) {
         CoroutineScope(Dispatchers.IO).launch {
             // todo
 //            repo.updateProviderStatus(1, status)
         }
+    }
+
+    fun ping(): LiveData<ResponseError> {
+        val data = MutableLiveData<ResponseError>()
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                data.postValue(generalRepository.ping())
+            }
+            catch (e: Exception) {
+                data.postValue(ResponseError("Unable to reach server", true))
+                Logger.d("Error in ping")
+                e.printStackTrace()            }
+        }
+        return data
     }
 }
