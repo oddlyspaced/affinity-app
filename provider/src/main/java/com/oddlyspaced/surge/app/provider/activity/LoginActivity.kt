12acc853ffab0afea62f7ccc6f2280432d283ae9
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-07 04:24:45 +0530

    provider: LoginActivity: use view binding

--- provider/src/main/java/com/oddlyspaced/surge/app/provider/activity/LoginActivity.kt
+++ provider/src/main/java/com/oddlyspaced/surge/app/provider/activity/LoginActivity.kt
@@ -3,10 +3,14 @@ package com.oddlyspaced.surge.app.provider.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.oddlyspaced.surge.app.provider.R
+import com.oddlyspaced.surge.app.provider.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
     }
 }
