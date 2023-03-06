c673ff35162a2fd5b4550cecd5f537fa25f0c7bc
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-06 14:53:49 +0530

    manager: add class files for recycler view swipe options

---
+++ manager/src/main/java/com/oddlyspaced/surge/manager/adapter/swipe/OnActivityTouchListener.java
@@ -0,0 +1,7 @@
+package com.oddlyspaced.surge.manager.adapter.swipe;
+
+import android.view.MotionEvent;
+
+public interface OnActivityTouchListener {
+    void getTouchCoordinates(MotionEvent ev);
+}
