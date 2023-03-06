d73c82c54d6fbe2f537aebb83dd338cc97bee106
oddlyspaced <oddlyspaced1@gmail.com>
2023-03-06 14:54:17 +0530

    manager: HomeFragment: test swipe for options in provider list

--- manager/src/main/java/com/oddlyspaced/surge/manager/fragment/HomeFragment.kt
+++ manager/src/main/java/com/oddlyspaced/surge/manager/fragment/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.oddlyspaced.surge.manager.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -10,12 +11,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.oddlyspaced.surge.app.common.modal.Provider
 import com.oddlyspaced.surge.manager.R
 import com.oddlyspaced.surge.manager.adapter.ProviderListAdapter
+import com.oddlyspaced.surge.manager.adapter.swipe.RecyclerTouchListener
 import com.oddlyspaced.surge.manager.databinding.FragmentHomeBinding
 import com.oddlyspaced.surge.manager.viewmodel.ManagerViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var binding: FragmentHomeBinding
     private val vm: ManagerViewModel by activityViewModels()
@@ -46,6 +48,28 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
                 layoutManager = LinearLayoutManager(requireContext())
                 adapter = ProviderListAdapter(providers, findNavController())
             }
+            val touchListener = RecyclerTouchListener(requireActivity(), binding.rvHomeProviderList)
+            touchListener.setClickable(object : RecyclerTouchListener.OnRowClickListener {
+                override fun onRowClicked(position: Int) {
+                }
+
+                override fun onIndependentViewClicked(independentViewID: Int, position: Int) {
+                }
+            })
+                .setSwipeOptionViews(R.id.delete_task, R.id.edit_task)
+                .setSwipeable(
+                    R.id.layout_item_provider_root, R.id.rowBG
+                ) { viewId, position ->
+                    when (viewId) {
+                        R.id.delete_task -> {
+                            Toast.makeText(requireContext(), "Not Available", Toast.LENGTH_SHORT).show()
+                        }
+                        R.id.edit_task -> {
+                            Toast.makeText(requireContext(), "Edit Not Available", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            binding.rvHomeProviderList.addOnItemTouchListener(touchListener)
         }
     }
 
